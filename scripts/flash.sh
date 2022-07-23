4c6f640ce5b30f271d5eaac4e669ae33f2758c1c
Alex Brinister <brinisal@tutamail.com>
2022-07-22 23:15:47 -0400

    added build/flash scripts; completed flash script

---
+++ scripts/flash.sh
@@ -0,0 +1,72 @@
+#! /usr/bin/env bash
+
+function usage() {
+  echo "Usage: $(basename $0) [-h] [-b <filename>] [-d <devicefile>]"
+  echo '     -h    shows the usage prompt'
+  echo '     -b    the binary file to flash to the Feather M4'
+  echo '     -d    the device file (in /dev) to flash the binary to'
+
+  exit 1
+}
+
+function check_for_bossa() {
+  if [[ ! -e `which bossac` ]]; then
+    echo "You do not have bossac installed!"
+    exit 1
+  fi 
+}
+
+function run_bossa() {
+  if ! check_for_bossa; then
+    exit 1
+  fi
+
+  bossac \
+    --port "$2" \
+    --offset 0x4000 \
+    --write \
+    --verify \
+    --reset \
+    --info \
+    --debug \
+    "$1"
+}
+
+if [[ ${#} -eq 0 ]]; then
+  usage
+fi
+
+optstring="hb:d:"
+
+while getopts ${optstring} arg; do
+  case "${arg}" in
+    h)
+      usage
+      ;;
+    b)
+      BINPATH="${OPTARG}"
+      ;;
+    d)
+      DEV="${OPTARG}"
+      ;;
+    ?)
+      echo "Invalid option: -${OPTARG}."
+      echo
+      usage
+      ;;
+  esac
+done
+
+# Makes sure files exist before passing them to bossac
+if [[ ! -e "${BINPATH}" ]]; then
+  echo "File ${BINPATH} does not exist"
+  exit 1
+fi
+
+if [[ ! -e "${DEV}" ]]; then
+  echo "Device ${DEV} does not exist"
+  exit 1
+fi
+
+# Run bossac
+run_bossa "${BINPATH}" "${DEV}"
